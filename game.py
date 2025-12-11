3b0942b705f5fd07e8a58083b96b935300e8b58d
jvlundsten-a11y <jvlundsten@gmail.com>
2025-12-11 02:49:21 +0000

    msg

---
+++ game.py
@@ -0,0 +1,9 @@
+print("Welcome to rock-paper-scissors!")
+
+choice = str(input("Choose rock, paper, or scissors: "))
+
+print("You chose:" , choice)
+
+poss = ["rock", "paper", "scissors"]
+choice = random.choice(poss)
+print(f"computer chose {choice}.")
